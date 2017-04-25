2c3b5ddd89345c658975dcb1644264fe5a4c7afa
chinmay_nagarkar <chinmay.nagarkar@gmail.com>
2017-04-24 23:09:24 -0800

    Adding first set of files for plasticsandpcbsposts

---
+++ sw4stm32/HelloWorld/App/application.c
@@ -0,0 +1,28 @@
+/*
+ * application.c
+ *
+ *  Created on: Apr 24, 2017
+ *      Author: chinm_000
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include "application.h"
+#include "usart.h"
+#include "stm32f4xx_hal_uart.h"
+
+static uint16_t BUF_LEN = 200;
+
+void App_Init() {
+App_Printf("Starting Board, Hello %s\r\n", "<your name here>");
+}
+
+void App_Printf(char const *format, ...) {
+va_list arg;
+va_start(arg, format);
+char buf[BUF_LEN];
+uint32_t len = vsnprintf(buf, sizeof(buf), format, arg);
+va_end(arg);
+len = LESS(len, sizeof(buf) - 1);
+HAL_UART_Transmit(&huart2, (uint8_t *) buf, len, 0xFFFF);
+}
---
+++ sw4stm32/HelloWorld/App/application.h
@@ -0,0 +1,17 @@
+/*
+ * application.h
+ *
+ *  Created on: Apr 24, 2017
+ *      Author: chinm_000
+ */
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+#define LESS(a_, b_) (((a_) < (b_)) ? (a_) : (b_))
+void App_Init(void);
+void App_Printf(char const *format, ...);
+
+
+
+#endif /* APPLICATION_H_ */
